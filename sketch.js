2f0cda41d2870373a77dc374a6d837ccd87e4220
cath levett <cathlevettgraphics@gmail.com>
2021-05-01 17:08:57 +0100

    canvas gen-art exported

--- sketch.js
+++ sketch.js
@@ -13,17 +13,18 @@ const sketch = () => {
 
   const createGrid = () => {
     const points = [];
-    const count = 30;
+    const count = 10;
 
     // Create grid
     for (let x = 0; x < count; x++) {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
-        const radius = Math.abs(random.noise2D(u, v)) * 0.025;
+        const radius = Math.abs(random.noise2D(u, v)) * 0.1;
         points.push({
           // radius: Math.abs(random.gaussian() * 0.025),
           radius,
+          rotation: random.noise2D(u, v),
           position: [u, v],
           color: random.pick(palette),
         });
@@ -33,26 +34,35 @@ const sketch = () => {
   };
 
   // random.setSeed(60);
-  const points = createGrid().filter(() => random.value() > 0.5);
+  const points = createGrid().filter(() => random.value() > 0.1);
   const margin = 400;
 
   return ({ context, width, height }) => {
-    // context.fillStyle = '#fff';
-    // context.fillRect(0, 0, width, height);
+    context.fillStyle = '#fff';
+    context.fillRect(0, 0, width, height);
 
     // Get px coords for grid points
     for (const data of points) {
-      const { position, radius, color } = data;
+      const { position, radius, color, rotation } = data;
       const [u, v] = position;
       // const x = u * width;
       // const y = v * height;
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
 
-      context.beginPath();
-      context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+      // context.beginPath();
+      // context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+      // context.fillStyle = color;
+      // context.fill();
+
+      context.save();
       context.fillStyle = color;
-      context.fill();
+      context.font = `${radius * width}px "JetBrains Mono"`;
+      context.translate(x, y);
+      context.rotate(rotation);
+      context.fillText('=', 0, 0);
+
+      context.restore();
     }
   };
 };
